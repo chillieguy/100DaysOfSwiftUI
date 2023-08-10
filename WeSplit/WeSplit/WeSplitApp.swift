d1824054d7573209502c5f0c0499cfa0f957d049
Chuck Underwood <chillieguy@live.com>
2023-08-10 10:00:02 -0700

    New project for WeSplit

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Chuck Underwood on 8/10/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
